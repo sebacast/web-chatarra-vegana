9c289099e6e26c691a9cdfa5ef2ee81c125c8b9e
Sebastian Castillo <sebacastriv@gmail.com>
2023-08-21 12:37:51 -0300

    improving menu js

--- assets/menu.js
+++ assets/menu.js
@@ -1,6 +1,21 @@
 const menuButton = document.getElementById('menu-header-h');
+const menuItemButtons = document.querySelectorAll('.menu-item > a');
+
+//evento click en menu hamburguesa
 menuButton.addEventListener('click', function(e) {
   e.preventDefault();
+  hideShowMenuItems();
+});
+//evento click en links del menu
+menuItemButtons.forEach(function(menuItemButton) {
+  menuItemButton.addEventListener('click', function(e) {
+    //e.preventDefault(); //mmm pegriloso, muy pegriloso!
+    hideShowMenuItems();
+  });
+});
+
+//función para mostrar/ocultar elementos del menu (busca por clase -> .menu-item)
+function hideShowMenuItems(){
   let menuItems = document.querySelectorAll(".menu-item");
   menuItems.forEach((item) => {
     if(!item.style.display || item.style.display == "none"){
@@ -10,4 +25,4 @@ menuButton.addEventListener('click', function(e) {
         item.style.display = "none"; 
     }
   });
-});
+}

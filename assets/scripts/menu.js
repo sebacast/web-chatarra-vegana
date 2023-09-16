0d9e8c55f807107b8b3a0fc01177d1a87eb8a81f
Sebastian Castillo <sebacastriv@gmail.com>
2023-09-16 01:53:50 -0300

    add categories + hero image in home + inner link animation

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,6 +1,7 @@
 $(function() {
   //evento click en menu hamburguesa
-  $(document).on('click', '#menu-header-h', function() {
+  $(document).on('click', '#menu-header-h', function(e) {
+    e.preventDefault();
     hideShowMenuItems();
   });
 
@@ -9,16 +10,46 @@ $(function() {
     hideShowMenuItems();
   });
 
+  $(document).on('click', '.menu-item', function(e) {
+    e.preventDefault();
+    let linkedSection = $(this).find('a').attr('href');
+    if (linkedSection) {
+      let menuHeight = 60;
+      let position = $(linkedSection).offset().top - menuHeight;
+      $('html, body').animate({
+        scrollTop: position
+      }, 1000); 
+    }
+  });
+
+  $(document).on("click", "#go-up-button", function (e) {
+    e.preventDefault();
+    $("html, body").animate({scrollTop: -60},1000);
+  });
+
 });
 
 //función para mostrar/ocultar elementos del menu (busca por clase -> .menu-item)
-function hideShowMenuItems() {
-  let menuItems = $('.menu-item');
-  menuItems.each(function() {
-    if (!$(this).is(':visible')) {
-      $(this).show();
-    } else {
-      $(this).hide();
+// function hideShowMenuItems() {
+//   let menuItems = $('.menu-item');
+//   menuItems.each(function() {
+//     $(this).show();
+//     if (!$(this).is(':visible')) {
+//       $(this).show();
+//     } else {
+//       $(this).hide();
+//     }
+//   });
+// }
+
+function hideShowMenuItems(){
+  let menuItems = document.querySelectorAll(".menu-item");
+  menuItems.forEach((item) => {
+    if(!item.style.display || item.style.display == "none"){
+        item.style.display = "block"; 
+    }
+    else{
+        item.style.display = "none"; 
     }
   });
 }
